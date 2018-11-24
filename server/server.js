8162afd1e8cca35e982f93106266e0718f2e926e
Long Nguyen <2006.long.nguyen@gmail.com>
2018-11-24 21:21:42 +1100

    CircleCI test 2

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send('Hey, UPDATED 2')
+    res.send('Hey, UPDATED AGAIN')
 })
 
 app.listen(3000, () => {
