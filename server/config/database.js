db9fdb3afe81002aa154381d1f506f1d9601c572
Long Nguyen <2006.long.nguyen@gmail.com>
2018-11-25 03:28:08 +1100

    Test of production MongoDB connection 5

--- server/config/database.js
+++ server/config/database.js
@@ -1,6 +1,6 @@
 // if production, use Mlab else use local db
 // if (process.env.NODE_ENV === "production"){
-    module.exports = { mongoURI: 'mongodb://long:Mernmade123@68.183.57.30:27017/hashtaghound'}
+    module.exports = { mongoURI: 'mongodb://long:Mernmade123@68.183.57.235:27017/hashtaghound'}
 // } else {
     // module.exports = { mongoURI: 'mongodb://localhost/hashtaghound' }
 // }
