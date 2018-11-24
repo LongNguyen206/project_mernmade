d5396a4c0e2c5ce4997f36a7d15534a7f0c42dd4
Long Nguyen <2006.long.nguyen@gmail.com>
2018-11-25 01:57:56 +1100

    Connect MongoDB, add ReactJS

---
+++ server/config/database.js
@@ -0,0 +1,6 @@
+// if production, use Mlab else use local db
+if (process.env.NODE_ENV === "production"){
+    module.exports = { mongoURI: 'mongodb://68.183.57.30:27017/hashtaghound'}
+} else {
+    module.exports = { mongoURI: 'mongodb://localhost/hashtaghound' }
+}
--- server/server.js
+++ server/server.js
@@ -1,10 +1,25 @@
 const express = require('express');
 const app = express();
+//require mongoose
+const mongoose = require('mongoose');
+//require the body parser to parse through the form content
+const bodyParser = require('body-parser');
+
+//include the database:
+const db = require('./config/database');
+
+mongoose.Promise = global.Promise;
+mongoose.connect(db.mongoURI, { useNewUrlParser: true })
+  .then(() => console.log('Connected to MongoDB'))
+  .catch((err) => console.log(err));
 
 app.get('/', (req, res) => {
     res.send('Hey, UPDATED AGAIN')
-})
+});
 
-app.listen(3000, () => {
-    console.log('Server is up on 3000')
-})
+//specify port
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => {
+    console.log(`Server is up on ${port}`)
+});
