e6835e8b5543e65ef6cc31d921d7f0687c47017d
Long Nguyen <2006.long.nguyen@gmail.com>
2018-11-25 03:33:47 +1100

    Establish connection to MongoDB droplet

--- server/config/database.js
+++ server/config/database.js
@@ -1,6 +1,5 @@
-// if production, use Mlab else use local db
-// if (process.env.NODE_ENV === "production"){
-    module.exports = { mongoURI: 'mongodb://long:Mernmade123@68.183.57.235:27017/hashtaghound'}
-// } else {
-    // module.exports = { mongoURI: 'mongodb://localhost/hashtaghound' }
-// }
+if (process.env.NODE_ENV === "production"){
+    module.exports = { mongoURI: 'mongodb://68.183.57.235:27017/hashtaghound'}
+} else {
+    module.exports = { mongoURI: 'mongodb://localhost/hashtaghound' }
+}
