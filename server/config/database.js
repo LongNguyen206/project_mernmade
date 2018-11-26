ff504560b9721b2c5c6de14e0f32ff396ee2770c
Long Nguyen <2006.long.nguyen@gmail.com>
2018-11-26 17:39:18 +1100

    Commit before pull

--- server/config/database.js
+++ server/config/database.js
@@ -1,5 +1,5 @@
 if (process.env.NODE_ENV === "production"){
-    module.exports = { mongoURI: 'mongodb://68.183.57.235:27017/hashtaghound'}
+    module.exports = { mongoURI: 'mongodb://68.183.57.235:27017/hashtaghound' }
 } else {
     module.exports = { mongoURI: 'mongodb://localhost/hashtaghound' }
 }
