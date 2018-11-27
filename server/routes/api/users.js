109700b8d56a8570b8b66e944daae8131e372361
Long Nguyen <2006.long.nguyen@gmail.com>
2018-11-27 12:11:58 +1100

    Fix the issue with taken email registartion

--- server/routes/api/users.js
+++ server/routes/api/users.js
@@ -5,41 +5,47 @@ const auth = require("../auth");
 const User = mongoose.model("users");
 
 //POST new user route (optional, everyone has access)
-router.post("/", auth.optional, (req, res) => {
-  const user = req.body;
-  console.log(user);
-
-  if (!user.name) {
-    return res.status(422).json({
-      errors: {
-        name: "is required"
-      }
-    });
-  }
-
-  if (!user.email) {
-    return res.status(422).json({
-      errors: {
-        email: "is required"
-      }
-    });
-  }
-
-  if (!user.password) {
-    return res.status(422).json({
-      errors: {
-        password: "is required"
-      }
-    });
-  }
-
-  const finalUser = new User(user);
-
-  finalUser.setPassword(user.password);
-
-  return finalUser
-    .save()
-    .then(() => res.json({ user: finalUser.toAuthJSON() }));
+router.post("/", auth.optional, (req, res) => {  
+  User.find({
+    email: req.body.email
+  })
+  .then(user => {
+    if (user.length != 0) {
+      return res.status(422).json({
+        errors: {
+          email: "is taken"
+        }
+      });
+    } 
+    if (!req.body.name) {
+      return res.status(422).json({
+        errors: {
+          name: "is required"
+        }
+      });
+    }
+    if (!req.body.email) {
+      return res.status(422).json({
+        errors: {
+          email: "is required"
+        }
+      });
+    }
+    if (!req.body.password) {
+      return res.status(422).json({
+        errors: {
+          password: "is required"
+        }
+      });
+    } else {
+      const finalUser = new User(req.body);
+      finalUser.setPassword(req.body.password);
+      return finalUser
+        .save()
+        .then(() => res.json({ user: finalUser.toAuthJSON() }));
+    }
+  })
+  .catch(err => console.log(err));
 });
 
 //POST login route (optional, everyone has access)
