15dd05f9ede27252a60cfa5080a5ce9055c1a21d
Long Nguyen <2006.long.nguyen@gmail.com>
2018-11-29 13:54:06 +1100

    Commit before push

--- server/routes/api/profile.js
+++ server/routes/api/profile.js
@@ -21,4 +21,26 @@ router.get('/', passport.authenticate('jwt', { session: false }), (req, res) => 
         .catch(err => res.status(404).json(err));
 });
 
+// @route   POST api/profile/
+// @desc    Create Current User's profile
+// @access  Private
+// router.post('/', passport.authenticate('jwt', { session: false }), (req, res) => {
+//     // Get fields
+//     const profileFields = {};
+//     profileFields.user = req.user.id;
+//     // if body went through, assign the value
+//     if (req.body.handle) profileFields.handle = req.body.handle;
+//     if (req.body.industry) profileFields.industry = req.body.industry;
+//     if (req.body.company) profileFields.company = req.body.company;
+//     if (req.body.website) profileFields.website = req.body.website;
+//     if (req.body.location) profileFields.location = req.body.location;
+//     Profile.findOne({ user: req.user.id })
+//         .then(profile => {
+//             if (profile) {
+//                 // Update
+//                 Profile.findOneAndUpdate({ user: req.})
+//             }
+//         })
+// });
+
 module.exports = router;
