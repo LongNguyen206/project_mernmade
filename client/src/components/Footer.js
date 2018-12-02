a0bb5e0f66d3d8b3df22bfdacaad53c40e4a43ac
Long Nguyen <2006.long.nguyen@gmail.com>
2018-12-03 10:16:22 +1100

    Replace MaterialUI with React-Materialize

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -1,60 +1,28 @@
-import Grid from '@material-ui/core/Grid'
-import { withStyles } from '@material-ui/core/styles'
-import Typography from '@material-ui/core/Typography'
-import React, { Component } from 'react'
-import { Link }  from  'react-router-dom';
+import React, { Component } from 'react';
+import { Footer } from 'react-materialize';
 
-
-class Footer extends Component {
-  render () {
-    const { classes } = this.props
-    const currentYear = new Date().getFullYear()
-    return (
-      <div className={classes.root}>
-        <Grid className={classes.subFooter} item xs={12}>
-          <Typography className={classes.white} variant="subheading" component={'span'} >
-          © {currentYear} Hashtag Hound
-          </Typography>
-          <Typography>
-           <Link to="/"> Terms of Use </Link>
-            <Link to="/"> Privacy Policy</Link>
-            <Link  to="/"> Contact Us </Link>
-          </Typography>
-        </Grid>
-      </div>
-    )
-  }
+class FooterComp extends Component {
+    render () {
+        return (
+            <Footer copyrights="&copy 2015 Copyright Text" moreLinks={
+                    <a className="grey-text text-lighten-4 right" href="#!">More Links</a>
+                }
+                links={
+                    <ul>
+                    <li><a className="grey-text text-lighten-3" href="#!">Link 1</a></li>
+                    <li><a className="grey-text text-lighten-3" href="#!">Link 2</a></li>
+                    <li><a className="grey-text text-lighten-3" href="#!">Link 3</a></li>
+                    <li><a className="grey-text text-lighten-3" href="#!">Link 4</a></li>
+                    </ul>
+                }
+                className='example'
+                >
+                <h5 className="white-text">Footer Content</h5>
+                <p className="grey-text text-lighten-4">You can use rows and columns here to organize your footer content.</p>
+            </Footer>
+          )
+    }
+  
 }
 
-
-
-const styles = theme => ({
-  root: {
-    marginTop: 30,
-    backgroundColor: `${theme.palette.primary[500]}`,
-    paddingTop: '30px',
-    overflowX: 'hidden'
-
-  },
-  footerSections: {
-    margin: '0 16px'
-  },
-  subFooter: {
-    backgroundColor: 'rgba(0, 0, 0, 0.15)',
-    padding: '8px 16px 8px 16px',
-    marginTop: '8px'
-  },
-  footerText: {
-    color: '#fff',
-    fontSize: '30px',
-    lineHeight: 1.5
-  },
-  white: {
-    color: '#ffffff'
-  },
-  flexContainer: {
-    display: 'flex'
-  }
-})
-
-export default withStyles(styles)(Footer)
+export default FooterComp;
--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,109 +1,25 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import { withStyles } from '@material-ui/core/styles';
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
-import Typography from '@material-ui/core/Typography';
-import Button from '@material-ui/core/Button';
-import IconButton from '@material-ui/core/IconButton';
-import Dialog from '@material-ui/core/Dialog';
-import DialogActions from '@material-ui/core/DialogActions';
-import DialogContent from '@material-ui/core/DialogContent';
-import DialogContentText from '@material-ui/core/DialogContentText';
-import DialogTitle from '@material-ui/core/DialogTitle';
-import FormControl from '@material-ui/core/FormControl';
-
-// Styling for our navbar
-const styles = {
-  root: {
-    flexGrow: 1,
-  },
-  grow: {
-    flexGrow: 1,
-  },
-  menuButton: {
-    marginLeft: -12,
-    marginRight: 20,
-  },
-  button: {
-    margin: 10,
-    fontSize: 17
-  },
-  text: {
-    textAlign: 'center',
-    fontSize: 20,
-
-    padding: '0.5em'
-  }
-};
+import React, { Component } from 'react';
+import { Navbar, NavItem } from 'react-materialize';
+import { Modal, Button, Row, Input } from 'react-materialize';
 
 
-
-// here is our navbar
-class Navbar extends React.Component{
-    state = {
-     open: false,
-   };
-
-   handleClickOpen = () => {
-     this.setState({ open: true });
-   };
-
-   handleClose = () => {
-     this.setState({ open: false });
-   };
-
-    render(){
-      const { classes } = this.props
-      return (
-    <div className={classes.root}>
-      <AppBar position="static" color="inherit">
-        <Toolbar>
-          <IconButton className={classes.menuButton} color="inherit" aria-label="Menu">
-
-          </IconButton>
-          <Typography variant="h6" color="inherit" className={classes.grow}>
-
-          </Typography>
-          <Button color="inherit" onClick={this.handleClickOpen}>Login</Button>
-
-        </Toolbar>
-      </AppBar>
-      <Dialog
-         open={this.state.open}
-         onClose={this.handleClose}
-         aria-labelledby="alert-dialog-title"
-         aria-describedby="alert-dialog-description">
-       <DialogTitle id="alert-dialog-title">{"Have an account? Log In"}</DialogTitle>
-         <DialogContent>
-           <DialogTitle>
-              <Button variant="contained" color="primary" className={classes.button}>
-                Sign In with Facebook </Button>
-                <Button variant="contained" color="primary" className={classes.button}>
-             Sign In with LinkedIn </Button>
-        </DialogTitle>
-       <DialogContentText className={classes.text}>
-               or
-      </DialogContentText>
-
-
-         </DialogContent>
-         <DialogActions>
-           <Button onClick={this.handleClose} color="primary" autoFocus>
-             Sign Up
-           </Button>
-         </DialogActions>
-       </Dialog>
-    </div>
-  );
-}
+class NavbarComp extends Component {
+    render () {
+        return (
+            <Navbar brand='logo' right>
+                <NavItem onClick={() => console.log('test click')}>Getting started</NavItem>
+                <NavItem>
+                    <Modal header='Modal Header' trigger={<div>Sign In</div>}>
+                        <Row>
+                            <Input type="email" label="Email" s={12} />
+                            <Input type="password" label="password" s={12} />                  
+                        </Row>
+                    </Modal>
+                </NavItem>
+            </Navbar>
+          )
+    }
+  
 }
 
-// requiring a class object
-Navbar.propTypes = {
-  classes: PropTypes.object.isRequired,
-};
-
-
-
-export default withStyles(styles)(Navbar);
+export default NavbarComp;
