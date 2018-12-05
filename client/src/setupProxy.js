be3b3f2974629f336507ab8646368bcbce175018
Mariam <mariam.antar@hotmail.com>
2018-12-06 10:37:42 +1100

    client folder excluded

--- client/src/setupProxy.js
+++
@@ -1,5 +0,0 @@
-const proxy = require("http-proxy-middleware");
-// import proxy from "http-proxy-middleware";
-module.exports = app => {
-  app.use(proxy("/api/users/auth/facebook", { target: "http://localhost:5000" }));
-};
