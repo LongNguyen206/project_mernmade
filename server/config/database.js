e75e91a6074f1c340a98a464c59c41daef5e6019
Long Nguyen <2006.long.nguyen@gmail.com>
2018-12-13 16:55:23 +1100

    Fix

--- server/config/database.js
+++ server/config/database.js
@@ -2,6 +2,6 @@ if (process.env.NODE_ENV === "production") {
   module.exports = { mongoURI: "mongodb://68.183.57.235:27017/hashtaghound" };
 } else {
   module.exports = {
-    mongoURI: "mongodb://Jesse:q12345@ds139425.mlab.com:39425/hashtag_hound"
+    mongoURI: process.env.MONGO_URI
   };
 }
